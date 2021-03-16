d173ac83c266fdeea0204a25290077e9ad9d6470
Naza <n.narkozhoeva@gmail.com>
2021-03-16 18:50:54 -0500

    Git

--- src/GitTest.java
+++ src/GitTest.java
@@ -3,5 +3,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("100_000.00$");
         System.out.println("100");
+        System.out.println("Naza");
     }
 }
